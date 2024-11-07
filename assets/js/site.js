94413e458d658a9628d4a89f0d2f509216ddd524
myungkim930 <myung.kim@inria.fr>
2024-11-08 00:41:44 +0100

    erase wechat

--- assets/js/site.js
+++ assets/js/site.js
@@ -39,29 +39,3 @@ $(document).ready(function() {
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
-
-/**
- * Moves WeChat widget
- */
-function moveWidget(event) {
-  var w = $("#wechat-widget");
-
-  w.css({
-    left: event.pageX - 25,
-    top: event.pageY - w.height() - 60
-  });
-}
-
-$("a#wechat-link").on("mouseenter", function(event) {
-  $("#wechat-widget").css({ display: "block" });
-
-  moveWidget(event);
-});
-
-$("a#wechat-link").on("mousemove", function(event) {
-  moveWidget(event);
-});
-
-$("a#wechat-link").on("mouseleave", function(event) {
-  $("#wechat-widget").css({ display: "none" });
-});
